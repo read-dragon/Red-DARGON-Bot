ea8451b8eaee1f93901ffa1c6edc863329ca7030
read-dragon <119781570+read-dragon@users.noreply.github.com>
2022-12-04 13:53:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,8 +29,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+94765690459'] // owner number
-global.ownername = "😎𝙒.𝘼.𝘼𝙎𝙃𝘼𝙉❯❯" // owner name
+global.vcardowner = ['+94715904356'] // owner number
+global.ownername = "Hashindu Kavinda" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
@@ -47,16 +47,16 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+94765690459']
-global.ownertag = '+94765690459' //your tag number
-global.botname = '𝘽𝙇𝙐𝙀-𝙒𝙊𝙇𝙁-𝘽𝙊𝙍' //ur bot name
+global.owner = ['+94715904356']
+global.ownertag = '+94715904356' //your tag number
+global.botname = 'DARKER BOT V2' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
 global.author = "𝘽𝙇𝙐𝙀-𝙒𝙊𝙇𝙁" // sticker author
 global.dragonemoji = '🌀' // Menu emoji
 global.footer = '[ blue-wolf ]' //
 
 //database
-global.premium = ['+94765690459'] //ur premium numbers
+global.premium = ['+94715904356'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
